26a688adf6de750f59ec567f36b73b62164a3b9a
vito.luce <vito.luce@finconsgroup.com>
2024-03-01 12:39:15 +0100

    primo commit!!!!

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
+        System.out.print(getMessage());
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
+    }
+    private static String getMessage(){
+        return "Buongiorno a tutti!\n";
     }
 }
