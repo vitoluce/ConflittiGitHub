1fc7945965275ccf1cf51a5b1ab6905606a68b9e
Fralog <francesco.logullo@finconsgroup.com>
2024-03-01 12:26:31 +0100

    First commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("Hello and welcome!");
+        System.out.println("What will be i after the cycle?");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
+            System.out.println("Now i is" + i);
         }
+
     }
 }
